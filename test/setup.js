d7a9d2cfc9d31f665bae08011a6d0ec5616d5a91
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-01-14 00:17:51 +0100

    fix: remove stale jest config

--- test/setup.js
+++ test/setup.js
@@ -1,15 +1,4 @@
-// Add globals here
-// ---
-// In the example below we're providing no-ops for the logging functions
-
 require("@testing-library/jest-dom");
 
+// HACK
 global.fetch = () => Promise.resolve();
-
-// global.console = {
-//   log: vi.fn(),
-//   error: vi.fn(),
-//   warn: vi.fn(),
-//   info: vi.fn(),
-//   debug: vi.fn(),
-// };
