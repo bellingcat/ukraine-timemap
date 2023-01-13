e9219a78d834452721d8c948db65839adead1a0e
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-01-13 17:27:14 +0100

    fix: remove `react-portal` dependency

--- src/components/space/carto/atoms/Regions.js
+++ src/components/space/carto/atoms/Regions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Portal } from "react-portal";
+import Portal from "../../../Portal";
 
 function MapRegions({ svg, regions, projectPoint, styles }) {
   function renderRegion(region) {
