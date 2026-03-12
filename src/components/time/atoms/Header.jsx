1111606128195e9c68fe3407951e71c06697bd25
Miguel Sozinho Ramalho <19508417+msramalho@users.noreply.github.com>
2026-03-12 14:45:31 +0000

    updates to timeline and notice (#86)

--- src/components/time/atoms/Header.jsx
+++ src/components/time/atoms/Header.jsx
@@ -14,8 +14,11 @@ const TimelineHeader = ({ title, from, to, onClick, hideInfo, resetTest, resetCl
         <p dangerouslySetInnerHTML={{ __html: title }} />
         <p>
           {d0} - {d1}
-          <small className="reset-button" onClick={() => resetClick() }><a className="cell">{ resetTest }</a></small>
+          <small className="reset-button" onClick={() => resetClick()}><a className="cell">{resetTest}</a></small>
         </p>
+        <div style={{ fontWeight: "400", textAlign: "center", marginTop: "5px" }}>
+          <i className="material-icons" style={{ color: "#FFA726", fontSize: "18px", verticalAlign: "middle" }}>warning</i> No verified incidents are being added after Aug 2025.
+        </div>
       </div>
     </div>
   );
